3fbe5c2d64e28c927b6870b5e45d0d54654ed3b3
Chernysheva Sofya <128961179+SChernysheva@users.noreply.github.com>
2024-03-13 01:06:36 +0300

    add transform + send to server

--- logger/client_logger/src/client_logger.cpp
+++ logger/client_logger/src/client_logger.cpp
@@ -1,10 +1,16 @@
 //#include <not_implemented.h>
-
 #include "../include/client_logger.h"
 #include <iostream>
 #include<vector>
-#include<algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <mqueue.h>
 #include <fstream>
+#define NAME "/QUEUE"
+#define PACKET_SIZE 10
 
 std::map<std::string, std::pair<std::ofstream *, size_t> > client_logger::streams =
     std::map<std::string, std::pair<std::ofstream *, size_t> >();
@@ -68,11 +74,35 @@ client_logger::~client_logger() noexcept
     }
 }
 
+struct my_message
+{
+    //pid_t id_process;
+    int count_packets;
+    int num_of_packet;
+    char text[100];
+    char file_path[50];
+    logger::severity sever;
+    int id_;
+}; 
+
 logger const *client_logger::log(
     const std::string &text,
     logger::severity severity) const noexcept
-{
-    std::string current_dt = current_datetime_to_string();
+{   
+    //получить очередь
+    //собрать структуру
+    //поместить в очередь
+    static int id_ = 0;
+    struct mq_attr attr;
+    mqd_t mq = mq_open(NAME, O_WRONLY);
+    if (mq == (mqd_t)-1) { // Проверка на успешное открытие очереди
+        perror("mq_open"); // Вывод ошибки, если открытие не удалось
+        exit(1); // Выход из программы с ошибкой
+    }
+    if (mq_getattr(mq, &attr) == -1) {
+        std::cout << "Ошибка получения атрибутов очереди сообщений." << std::endl;
+    }
+    //std::string current_dt = current_datetime_to_string();
     for (auto & logger_stream : logger_streams)
     {
         if ( std::find(logger_stream.second.second.begin(), logger_stream.second.second.end(), severity) == logger_stream.second.second.end())
@@ -82,14 +112,73 @@ logger const *client_logger::log(
 
         if (logger_stream.second.first == nullptr)
         {
-            std::cout << "[" << severity_to_string(severity) << "]" 
-            << "[" << current_dt << "] " << text << std::endl;
-        }
+            int message_size = std::strlen(text.c_str());
+            int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; // Вычисляем количество пакетов
+            for (int i = 0; i < num_packets; i++)
+            {
+                my_message msg;
+
+                char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
+                int startIndex = i * PACKET_SIZE;
+                int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
+
+                memcpy(packet, &text[startIndex], endIndex - startIndex);
+                packet[endIndex - startIndex] = '\0'; // Завершаем строку null-символом
+                strcpy(msg.text, packet);
+                strcpy(msg.file_path,"console");
+                //pid_t pid = getpid(); //убунту
+                //msg.id_process = getpid();
+                msg.num_of_packet = i + 1;
+                msg.count_packets = num_packets;
+                msg.sever = severity;
+                msg.id_ = id_;
+                if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
+                {
+                    perror("mq_send");
+                    mq_close(mq);
+                    exit(1);
+                }
+            }
+            id_++;
+                // std::cout << "[" << severity_to_string(severity) << "]"
+                // << "[" << current_dt << "] " << text << std::endl;
+            }
         else
         {
-            (*logger_stream.second.first) << "[" << severity_to_string(severity) << "]" << "[" << current_dt << "] " << text << std::endl;
+            std:: cout << "sever " << severity_to_string(severity) << "find in " << logger_stream.first << std::endl;
+            int message_size = std::strlen(text.c_str());
+            int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; // Вычисляем количество пакетов
+            for (int i = 0; i < num_packets; i++)
+            {
+                my_message msg;
+
+                char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
+                int startIndex = i * PACKET_SIZE;
+                int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
+
+                memcpy(packet, &text[startIndex], endIndex - startIndex);
+                packet[endIndex - startIndex] = '\0'; // Завершаем строку null-символом
+                strcpy(msg.text, packet);
+                strcpy(msg.file_path, logger_stream.first.c_str());
+                std::cout << "file : " << msg.file_path << std::endl;
+                //msg.id_process = getpid();
+                msg.num_of_packet = i + 1;
+                msg.count_packets = num_packets;
+                msg.sever = severity;
+                msg.id_ = id_;
+                if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
+                {
+                    perror("mq_send");
+                    mq_close(mq);
+                    exit(1);
+                }
+            }
+            id_++;
+            
+            //(*logger_stream.second.first) << "[" << severity_to_string(severity) << "]" << "[" << current_dt << "] " << text << std::endl;
         }
     }
+    mq_close(mq);
 
     return this;
 }
--- logger/client_logger/src/client_logger_builder.cpp
+++ logger/client_logger/src/client_logger_builder.cpp
@@ -21,20 +21,29 @@ logger_builder *client_logger_builder::add_console_stream(
     logger::severity severity)
 {
     data["console"].push_back(severity);
-
     return this; 
 }
 
+
 logger_builder* client_logger_builder::transform_with_configuration(
-    std::string const &configuration_file_path,
-    std::string const &configuration_path)
+    std::string const &configuration_file_path, std::string const &configuration_path) //добавить путь
 {
-    //throw not_implemented("logger_builder* client_logger_builder::transform_with_configuration(std::string const &configuration_file_path, std::string const &configuration_path)", "your code should be here...");
+    //std::cout << configuration_file_path << std::endl;
+    std::ifstream fin(configuration_file_path);
+    auto info = nlohmann::json::parse(fin);
+    auto pairs = info[configuration_path];
+
+    //std::cout<< pairs << std::endl;
+    for(auto& elem : pairs){
+        data[elem.value("file", "Not found!")].push_back( elem["severity"]);
+    }
+
+    return this;
 }
 
 logger_builder *client_logger_builder::clear()
 {
-    //throw not_implemented("logger_builder *client_logger_builder::clear()", "your code should be here...");
+    data.clear();
 }
 
 logger *client_logger_builder::build() const

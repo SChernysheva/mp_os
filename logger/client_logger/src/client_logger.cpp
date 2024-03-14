ca8f778896fcc5603cac13dbbe0d2906996802fa
Chernysheva Sonya <128961179+SChernysheva@users.noreply.github.com>
2024-03-14 19:30:20 +0300

    add message queue + format str

--- logger/client_logger/src/client_logger.cpp
+++ logger/client_logger/src/client_logger.cpp
@@ -7,10 +7,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <mqueue.h>
 #include <fstream>
 #define NAME "/QUEUE"
 #define PACKET_SIZE 10
+//#define IMPLEMENTATION_FOR_UNIX 1
+ //#define IMPLEMENTATION_FOR_WIN 1
+#define IMPLEMENTATION_CLIENT 1
 
 std::map<std::string, std::pair<std::ofstream *, size_t> > client_logger::streams =
     std::map<std::string, std::pair<std::ofstream *, size_t> >();
@@ -25,8 +27,9 @@ std::map<logger::severity, std::string> client_logger::severity_to_str = {
 };
 
 client_logger::client_logger(
-    std::map<std::string, std::vector<logger::severity>> const & targets)
+    std::map<std::string, std::vector<logger::severity>> const & targets, std::string const& opt)
 {
+    this->opt = opt;
     for (auto & target : targets) //идем по парам (файл(консоль) - северити)
     {
         auto global_stream = streams.find(target.first);
@@ -76,7 +79,7 @@ client_logger::~client_logger() noexcept
 
 struct my_message
 {
-    //pid_t id_process;
+    char opt[25];
     int count_packets;
     int num_of_packet;
     char text[100];
@@ -85,100 +88,179 @@ struct my_message
     int id_;
 }; 
 
-logger const *client_logger::log(
-    const std::string &text,
-    logger::severity severity) const noexcept
-{   
-    //получить очередь
-    //собрать структуру
-    //поместить в очередь
-    static int id_ = 0;
-    struct mq_attr attr;
-    mqd_t mq = mq_open(NAME, O_WRONLY);
-    if (mq == (mqd_t)-1) { // Проверка на успешное открытие очереди
-        perror("mq_open"); // Вывод ошибки, если открытие не удалось
-        exit(1); // Выход из программы с ошибкой
-    }
-    if (mq_getattr(mq, &attr) == -1) {
-        std::cout << "Ошибка получения атрибутов очереди сообщений." << std::endl;
-    }
-    //std::string current_dt = current_datetime_to_string();
-    for (auto & logger_stream : logger_streams)
+#if defined(IMPLEMENTATION_FOR_UNIX) //send to server by message queue
+#include <mqueue.h>
+    logger const *client_logger::log(
+        const std::string &text,
+        logger::severity severity) const noexcept
     {
-        if ( std::find(logger_stream.second.second.begin(), logger_stream.second.second.end(), severity) == logger_stream.second.second.end())
+        static int id_ = 0;
+        struct mq_attr attr;
+        mqd_t mq = mq_open(NAME, O_WRONLY); //get queue
+        if (mq == (mqd_t)-1)
         {
-            continue;
+            perror("mq_open");
+            exit(1);
         }
-
-        if (logger_stream.second.first == nullptr)
+        for (auto &logger_stream : logger_streams)
         {
-            int message_size = std::strlen(text.c_str());
-            int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; // Вычисляем количество пакетов
-            for (int i = 0; i < num_packets; i++)
+            if (std::find(logger_stream.second.second.begin(), logger_stream.second.second.end(), severity) == logger_stream.second.second.end())
             {
-                my_message msg;
-
-                char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
-                int startIndex = i * PACKET_SIZE;
-                int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
-
-                memcpy(packet, &text[startIndex], endIndex - startIndex);
-                packet[endIndex - startIndex] = '\0'; // Завершаем строку null-символом
-                strcpy(msg.text, packet);
-                strcpy(msg.file_path,"console");
-                //pid_t pid = getpid(); //убунту
-                //msg.id_process = getpid();
-                msg.num_of_packet = i + 1;
-                msg.count_packets = num_packets;
-                msg.sever = severity;
-                msg.id_ = id_;
-                if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
-                {
-                    perror("mq_send");
-                    mq_close(mq);
-                    exit(1);
-                }
+                continue;
             }
-            id_++;
-                // std::cout << "[" << severity_to_string(severity) << "]"
-                // << "[" << current_dt << "] " << text << std::endl;
-            }
-        else
-        {
-            std:: cout << "sever " << severity_to_string(severity) << "find in " << logger_stream.first << std::endl;
-            int message_size = std::strlen(text.c_str());
-            int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; // Вычисляем количество пакетов
-            for (int i = 0; i < num_packets; i++)
+
+            if (logger_stream.second.first == nullptr) //if console
             {
-                my_message msg;
-
-                char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
-                int startIndex = i * PACKET_SIZE;
-                int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
-
-                memcpy(packet, &text[startIndex], endIndex - startIndex);
-                packet[endIndex - startIndex] = '\0'; // Завершаем строку null-символом
-                strcpy(msg.text, packet);
-                strcpy(msg.file_path, logger_stream.first.c_str());
-                std::cout << "file : " << msg.file_path << std::endl;
-                //msg.id_process = getpid();
-                msg.num_of_packet = i + 1;
-                msg.count_packets = num_packets;
-                msg.sever = severity;
-                msg.id_ = id_;
-                if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
+                int message_size = std::strlen(text.c_str());
+                int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; 
+                for (int i = 0; i < num_packets; i++)
                 {
-                    perror("mq_send");
-                    mq_close(mq);
-                    exit(1);
+                    my_message msg;
+                    strcpy(msg.opt, this->opt.c_str());
+                    char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
+                    int startIndex = i * PACKET_SIZE;
+                    int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
+
+                    memcpy(packet, &text[startIndex], endIndex - startIndex);
+                    packet[endIndex - startIndex] = '\0'; 
+                    strcpy(msg.text, packet);
+                    strcpy(msg.file_path, "console");
+                    // msg.id_process = getpid();
+                    msg.num_of_packet = i + 1;
+                    msg.count_packets = num_packets;
+                    msg.sever = severity;
+                    msg.id_ = id_;
+                    if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
+                    {
+                        perror("mq_send");
+                        mq_close(mq);
+                        exit(1);
+                    }
                 }
+                id_++;
             }
-            id_++;
-            
-            //(*logger_stream.second.first) << "[" << severity_to_string(severity) << "]" << "[" << current_dt << "] " << text << std::endl;
+            else //if file
+            {
+                int message_size = std::strlen(text.c_str());
+                int num_packets = (message_size + PACKET_SIZE - 1) / PACKET_SIZE; // Вычисляем количество пакетов
+                for (int i = 0; i < num_packets; i++)
+                {
+                    my_message msg;
+                    strcpy(msg.opt, this->opt.c_str());
+                    char packet[PACKET_SIZE + 1]; // +1 для завершающего символа null
+                    int startIndex = i * PACKET_SIZE;
+                    int endIndex = std::min(startIndex + PACKET_SIZE, message_size);
+
+                    memcpy(packet, &text[startIndex], endIndex - startIndex);
+                    packet[endIndex - startIndex] = '\0'; // Завершаем строку null-символом
+                    strcpy(msg.text, packet);
+                    strcpy(msg.file_path, logger_stream.first.c_str());
+                    // msg.id_process = getpid();
+                    msg.num_of_packet = i + 1;
+                    msg.count_packets = num_packets;
+                    msg.sever = severity;
+                    msg.id_ = id_;
+                    if (mq_send(mq, (char *)&msg, sizeof(my_message), 0) == -1)
+                    {
+                        perror("mq_send");
+                        mq_close(mq);
+                        exit(1);
+                    }
+                }
+                id_++;
+            }
         }
+        mq_close(mq);
+
+        return this;
     }
-    mq_close(mq);
 
-    return this;
-}
+#elif defined(IMPLEMENTATION_CLIENT)
+
+    logger const *client_logger::log(
+        const std::string &text,
+        logger::severity severity) const noexcept
+    {
+        for (auto &logger_stream : logger_streams)
+        {
+            if (std::find(logger_stream.second.second.begin(), logger_stream.second.second.end(), severity) == logger_stream.second.second.end())
+            {
+                continue;
+            }
+
+            if (logger_stream.second.first == nullptr)
+            {
+                for (int i = 0; opt[i] != '\0' && opt[i + 1] != '\0'; i++)
+                {
+                    if (opt[i] != '%') std::cout << opt[i];
+
+                    if (opt[i] == '%' && opt[i + 1] == 'd') 
+                    {
+                       std::cout << "[" << logger::current_date_to_string() << "]";
+                       i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 't') 
+                    {
+                       std::cout << "[" << logger::current_time_to_string() << "]";
+                       i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 's') 
+                    {
+                       std::cout << "[" << severity_to_string(severity) << "]";
+                       i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 'm') 
+                    {
+                       std::cout << text << " ";
+                       i++;
+                    }
+                    else if (opt[i] == '%') std::cout << "%";
+                }
+                std::cout << "\n";
+            }
+            else
+            {
+                for (int i = 0; opt[i] != '\0' && opt[i + 1] != '\0'; i++)
+                {
+                    if (opt[i] != '%')
+                        (*logger_stream.second.first)  << opt[i];
+
+                    if (opt[i] == '%' && opt[i + 1] == 'd')
+                    {
+                        (*logger_stream.second.first)  << "[" << logger::current_date_to_string() << "]";
+                        i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 't')
+                    {
+                        (*logger_stream.second.first)  << "[" << logger::current_time_to_string() << "]";
+                        i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 's')
+                    {
+                        (*logger_stream.second.first)  << "[" << severity_to_string(severity) << "]";
+                        i++;
+                    }
+                    else if (opt[i] == '%' && opt[i + 1] == 'm')
+                    {
+                        (*logger_stream.second.first)  << text << " ";
+                        i++;
+                    }
+                    else if (opt[i] == '%')
+                        (*logger_stream.second.first)  << "%";
+                }
+                (*logger_stream.second.first)  << "\n";
+            }
+        }
+
+        return this;
+    }
+#elif defined(IMPLEMENTATION_FOR_WIN)
+
+logger const *client_logger::log(
+        const std::string &text,
+        logger::severity severity) const noexcept
+    {}
+
+
+
+#endif

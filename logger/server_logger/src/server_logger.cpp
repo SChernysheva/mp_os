df319c0a7e076040e07b117561ba5ab21c996827
Chernysheva Sonya <128961179+SChernysheva@users.noreply.github.com>
2024-03-16 12:17:32 +0300

    add windows implementation

--- logger/server_logger/src/server_logger.cpp
+++ logger/server_logger/src/server_logger.cpp
@@ -128,8 +128,6 @@ void server_logger::init(void)
 
                         file.close();
                         std::remove(file_path_cash.c_str()); // remove cash file
-                        file.close();
-                        std::remove(file_path_cash.c_str());
                     }
                 }
             }
@@ -159,8 +157,102 @@ void server_logger::init(void)
 
 
 #elif _WIN32
+#include <windows.h>
 void server_logger::init(void)
-{}
+{
+    HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(my_message), "SharedMemory");
+    LPVOID pData = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(my_message));
+    HANDLE hSemaphore = CreateSemaphore(NULL, 1, 1, "Semaphore");
+
+    if (hMapFile && pData && hSemaphore)
+    {
+        while (true)
+        {
+            WaitForSingleObject(hSemaphore, INFINITE);
+            my_message *received_msg = (my_message *)pData;
+            if (received_msg->num_of_packet < 1) 
+            {
+                ReleaseSemaphore(hSemaphore, 1, NULL);   
+                continue;
+            }
+            if (received_msg->count_packets == received_msg->num_of_packet)
+            {
+                std::string current_dt = logger::current_datetime_to_string();
+                if (received_msg->count_packets == 1) // if there is only 1 packet for this text/file
+                {
+                    if (strcmp(received_msg->file_path, "console") == 0)
+                    {
+                        std::cout << "[" << current_dt << "]"
+                                  << "[" << severity_to_string(received_msg->sever) << "] " << received_msg->text << std::endl;
+                    }
+                    else
+                    {
+                        std::fstream str(received_msg->file_path, std::ios::in | std::ios::out | std::ios::app);
+                        str << "[" << current_dt << "]"
+                            << "[" << severity_to_string(received_msg->sever) << "] " << received_msg->text << std::endl;
+                    }
+                }
+                else
+                {
+                    std::string file_path_cash = "file_path_cash_id" + std::to_string(received_msg->id_) + ".txt"; // (if last packet) find a file for cash info from packets
+                    std::fstream file(file_path_cash, std::ios::in | std::ios::out | std::ios::app);
+                    if (file.is_open())
+                    {
+                        file << received_msg->text; // write info in file
+                    }
+                    file.seekg(std::ios::beg); // back to the start
+                    if (strcmp(received_msg->file_path, "console") == 0)
+                    {
+                        std::cout << "[" << current_dt << "]"
+                                  << "[" << severity_to_string(received_msg->sever) << "] ";
+                        std::string line;
+                        while (std::getline(file, line)) // get info from cash file and write it
+                        {
+                            std::cout << line << std::endl;
+                        }
+                        file.close();
+                        std::remove(file_path_cash.c_str()); // remove cash file
+                    }
+                    else
+                    {
+                        std::fstream str(received_msg->file_path, std::ios::in | std::ios::out | std::ios::app); // in file from file
+                        str << "[" << current_dt << "]"
+                            << "[" << severity_to_string(received_msg->sever) << "] ";
+                        std::string line;
+                        while (std::getline(file, line)) // get info from cash file and write it
+                        {
+                            str << line << std::endl;
+                        }
+                        file.close();
+                        std::remove(file_path_cash.c_str()); // remove cash file
+                    }
+                   }   
+            }
+            else
+            {
+                if (received_msg->num_of_packet == 1) // if there is first packet and count_packets > 1
+                {
+                    // make a new cash file and write
+                    std::string file_path_cash = "file_path_cash_id" + std::to_string(received_msg->id_) + ".txt";
+                    std::fstream file(file_path_cash, std::ios::in | std::ios::out | std::ios::app);
+                    file << received_msg->text;
+                }
+                else
+                {
+                    std::string file_path_cash = "file_path_cash_id" + std::to_string(received_msg->id_) + ".txt";
+                    std::ofstream file(file_path_cash, std::ios::app);
+                    file << received_msg->text;
+                }
+            }
+            // Обработка полученного сообщения
+            ReleaseSemaphore(hSemaphore, 1, NULL);
+        }
+
+        UnmapViewOfFile(pData);
+        CloseHandle(hMapFile);
+        CloseHandle(hSemaphore);
+    }
+}
 
 #endif
 logger const *server_logger::log(
